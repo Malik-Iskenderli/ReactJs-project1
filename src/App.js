ebd93d733bc4af5b293f9be7acb61c6291fe5dee
Malik-Iskenderli <122275697+Malik-Iskenderli@users.noreply.github.com>
2023-05-08 18:13:44 +0400

    Theme mode

--- src/App.js
+++ src/App.js
@@ -1,23 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./Components/Navbar";
+import Home from "./Components/Routers/Home";
+import Form from "./Components/Routers/Form";
+import Users from "./Components/Routers/Users";
+import Employe from "./Components/Routers/Employe";
+import Product from "./Components/Routers/Product";
+import React, { useEffect, useState } from "react";
+import { Routes,Route, json } from "react-router-dom";
+import "./Components/Sass/App.scss"
+
+
+
 
 function App() {
+
+  //reng modunun deismesinin State-i
+  const [theme, setTheme] = useState(JSON.parse(localStorage.getItem("theme")))
+
+  useEffect(()=>{
+    localStorage.setItem("theme" , JSON.stringify(theme))
+  },[theme])
+  
+  const Tabs =[
+   {
+     path: "/home",
+     element:<Home/>,
+     name: "Home",
+     icon: <i className="bi bi-house"></i>
+  },
+  {
+    path: "/form",
+    element: <Form/>,
+    name:"Form",
+    icon: <i className="bi bi-archive"></i>
+  },
+  {
+    path: "/users",
+    element: <Users/>,
+    name:"Users",
+    icon: <i className="bi bi-people"></i>
+  },
+  {
+    path: "/employe",
+    element: <Employe/>,
+    name:"Employe",
+    icon: <i className="bi bi-bar-chart-line"></i>
+  },
+  {
+    path: "/product",
+    element: <Product/>,
+    name:"Product",
+    icon: <i className="bi bi-shop"></i>
+  }
+  
+  
+  ]
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={theme ? "dark" : ""}>
+    
+
+    <Navbar Tabs={Tabs}  theme= {theme} setTheme={setTheme}/>
+    
+    <Routes>
+      {
+        Tabs.map((item,key)=>(
+        
+          <>
+          
+          <Route  path={item.path} element={item.element}/>
+
+          </>
+        ))
+      }
+    </Routes>
+
+    
+
+
     </div>
   );
 }
---
+++ src/Components/Navbar.js
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import './Sass/Navbar.scss'
+
+const Navbar = ({ Tabs, theme, setTheme }) => {
+
+
+    //reng modunun deismesinin functionu
+    const themeMode = () => {
+        setTheme(!theme)
+    }
+    return (
+        <nav className="navbar navbar-expand-lg headerr">
+            <div className="container-fluid header-children">
+
+                <a className="navbar-brand" href="#">Fogito</a>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+
+                <div className="collapse navbar-collapse link-div" id="navbarNav">
+                    <ul className="navbar-nav">
+
+                        {
+                            Tabs.map((item, key) => {
+                                return (
+                                    <li className='nav-item'>
+                                        <Link to={item.path} className="nav-link atag">
+                                            {item.icon}
+                                            <span>{item.name}</span>
+                                        </Link>
+
+                                    </li>
+                                )
+                            })
+                        }
+
+                    </ul>
+                </div>
+
+                <div>
+                    <button className="theme-mode"
+                        onClick={() => {
+                            themeMode()
+                        }
+                        }
+                    >
+                        <i class="bi bi-palette" />
+                    </button>
+                </div>
+
+               
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
